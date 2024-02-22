2dab1e79e71d37152ec8d78f3226a7e669ae9516
AndreMBarreira <andremilla.am@gmail.com>
2024-02-22 16:35:48 -0300

    Pageable list product

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,16 +1,15 @@
 package com.devsuperior.dscommerce.controllers;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscommerce.DTO.ProductDTO;
-import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.services.ProductService;
 
 @RestController
@@ -26,8 +25,8 @@ public class ProductController {
 	}
 	
 	@GetMapping()
-	public List<ProductDTO> findAll() {
-		return service.findAll();
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 
 }
